0f20d12a096b0a9bfc2b9e360ceec58ba0721614
Hahalek <agata.machalek@gmail.com>
2024-03-27 16:38:10 +0100

    When a laser hits an enemy they both disappear.

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -13,3 +13,8 @@ func _process(_delta):
 	var direction = Vector2.RIGHT
 	velocity = direction * speed
 	move_and_slide()
+
+
+func hit():
+	print("ouchie")
+	queue_free()
--- scenes/projectiles/laser.gd
+++ scenes/projectiles/laser.gd
@@ -3,6 +3,7 @@ extends Area2D
 @export var speed: int = 2300
 var direction: Vector2 = Vector2.UP
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,3 +12,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position += direction * speed * delta
+
+
+func _on_body_entered(body):
+	if "hit" in body:
+		body.hit()
+	queue_free()
